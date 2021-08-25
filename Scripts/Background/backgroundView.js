e65bdb1130835ffba1f78452c049ce06acfcdb8d
graceandstyle <suprahju01@gmail.com>
2021-08-25 13:10:45 +0800

    Release 1.0
    Release 1.0

--- Scripts/Background/backgroundView.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(() => {
-    initializeBackground();
-});
-
-function initializeBackground() {
-    backgroundPanel = initializeElement(htmlSection, backgroundPanel, [backgroundClassNames.backgroundPanel], mainDiv);
-    backgroundEllipse = initializeElement(htmlSection, backgroundEllipse, [backgroundClassNames.backgroundEllipse, backgroundClassNames.backgroundEllipseTopRight], backgroundPanel);
-    backgroundEllipse = initializeElement(htmlSection, backgroundEllipse, [backgroundClassNames.backgroundEllipse, backgroundClassNames.backgroundEllipseBottomLeft], backgroundPanel);
-}
