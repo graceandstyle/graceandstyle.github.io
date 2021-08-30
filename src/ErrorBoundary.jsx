070d0eeda1f10bdc19f08c3d2b49f1f7a85fa6e5
graceandstyle <suprahju01@gmail.com>
2021-08-31 00:02:28 +0800

    Release 1.3
    Release 1.3

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Icon from './images/ms-icon-150x150.png';
+
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 export default class ErrorBoundary extends React.Component {
     constructor(props) {
@@ -14,7 +17,52 @@ export default class ErrorBoundary extends React.Component {
     render() {
       if (this.state.hasError) {
         // You can render any custom fallback UI
-        return <h1>Something went wrong.</h1>;
+        return (
+          <div className="loaderdiv">
+            <div className="loadercontainer">
+                <img src={Icon} alt="Grace-Style-Logo"></img>
+                <div style={{whiteSpace:'nowrap',marginBottom:'10px'}}>Browser not supported.</div>
+                {
+                  (!isMobile) &&
+                    <>
+                      <div style={{whiteSpace:'nowrap',fontSize:'10pt',marginBottom:'10px'}}>Please use the following browser</div>
+                      <div style={{whiteSpace:'nowrap',width:'auto',height:'auto'}}>
+                        <div style={{position:'relative',width:'auto',height:'auto',display:'inline-block',verticalAlign:'top',marginRight:'10px'}}>
+                          <div className='fab fa-chrome addtooltip' style={{fontSize:'35pt',cursor:'pointer'}}>
+                            <div className="tooltip addshadow">
+                                <div></div>
+                                <span>Chrome</span>
+                            </div>
+                          </div>
+                        </div>
+                        <div style={{position:'relative',width:'auto',height:'auto',display:'inline-block',verticalAlign:'top',marginRight:'10px'}}>
+                          <div className='fab fa-edge addtooltip' style={{fontSize:'35pt',cursor:'pointer'}}>
+                            <div className="tooltip addshadow">
+                                <div></div>
+                                <span>Edge</span>
+                            </div>
+                          </div>
+                        </div>
+                        <div style={{position:'relative',width:'auto',height:'auto',display:'inline-block',verticalAlign:'top'}}>
+                          <div className='fab fa-firefox-browser addtooltip' style={{fontSize:'35pt',cursor:'pointer'}}>
+                            <div className="tooltip addshadow">
+                                <div></div>
+                                <span>Firefox</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                }
+                {
+                  (isMobile) &&
+                    <>
+                      <div style={{whiteSpace:'nowrap',fontSize:'10pt',marginBottom:'10px'}}>Please use a newer browser or newer mobile device.</div>
+                    </>
+                }
+            </div>
+        </div>
+        );
       }
   
       return this.props.children; 
